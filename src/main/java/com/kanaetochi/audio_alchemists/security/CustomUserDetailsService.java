ff1332a73228ca8983a073b3363cb4c41a1b143c
okiyitooo <kanaetochi.okiyi@udc.edu>
2025-01-29 14:17:21 -0500

    implement custom user details service and JWT authentication filter; update application properties for server port

--- src/main/java/com/kanaetochi/audio_alchemists/security/CustomUserDetailsService.java
+++ src/main/java/com/kanaetochi/audio_alchemists/security/CustomUserDetailsService.java
@@ -1,19 +1,20 @@
 package com.kanaetochi.audio_alchemists.security;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.kanaetochi.audio_alchemists.repository.UserRepository;
 
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService{
     private UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
+    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found with username/email: " + username));
