a0c0da813c2be90e492b57420ec3c2304d322d67
okiyitooo <kanaetochi.okiyi@udc.edu>
2025-02-01 13:10:36 -0500

    add SimpMessagingTemplate bean for WebSocket messaging support

--- src/main/java/com/kanaetochi/audio_alchemists/AudioAlchemistsApplication.java
+++ src/main/java/com/kanaetochi/audio_alchemists/AudioAlchemistsApplication.java
@@ -2,12 +2,20 @@ package com.kanaetochi.audio_alchemists;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @SpringBootApplication
 public class AudioAlchemistsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AudioAlchemistsApplication.class, args);
+	}
+
+	@Bean
+	public SimpMessagingTemplate messagingTemplate(MessageChannel messageChannel) {
+		return new SimpMessagingTemplate(messageChannel);
 	}
 
 }
